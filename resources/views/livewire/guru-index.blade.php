eaf7089b32fa95046264ed22bdd4f4889ea0e736
wijayy <mertawijaya77@gmail.com>
2026-01-04 21:36:25 +0800

    remove feature delete

--- resources/views/livewire/guru-index.blade.php
+++ resources/views/livewire/guru-index.blade.php
@@ -6,28 +6,28 @@
     </div>
     <div class="overflow-x-auto mt-4">
         <div class="flex gap-4 min-w-3xl font-semibold text-xs md:text-sm mt-4 py-2">
-            <div class="w-10">#</div>
-            <div class="w-1/7">Nama Guru</div>
-            <div class="w-1/7 text-center">Kode Guru</div>
-            <div class="w-1/7 text-center">Mata Pelajaran</div>
-            <div class="w-1/7 text-center">No Pegawai</div>
-            <div class="w-1/7 text-center">No Telepon</div>
-            <div class="w-1/7 text-center">Alamat</div>
+            <div class="w-10 ">#</div>
+            <div class="w-1/7 ">Nama Guru</div>
+            <div class="w-1/7 text-center ">Kode Guru</div>
+            <div class="w-1/7 text-center ">Mata Pelajaran</div>
+            <div class="w-1/7 text-center ">No Pegawai</div>
+            <div class="w-1/7 text-center ">No Telepon</div>
+            <div class="w-1/7 text-center ">Alamat</div>
             @if (Auth::user()->role == 'admin')
-                <div class="w-1/7 text-center">Aksi</div>
+                <div class="w-1/7 text-center ">Aksi</div>
             @endif
         </div>
         @foreach ($guru as $item)
             <div class="flex gap-4 min-w-3xl text-xs md:text-sm py-1">
-                <div class="w-10">{{ $loop->iteration }}</div>
-                <div class="w-1/7">{{ $item->name }}</div>
-                <div class="w-1/7 text-center">{{ $item->kode }}</div>
-                <div class="w-1/7 text-center">{{ $item->mapel?->nama }}</div>
-                <div class="w-1/7 text-center">{{ $item->no_pegawai ?? '-' }}</div>
-                <div class="w-1/7 text-center">{{ $item->no_telp }}</div>
-                <div class="w-1/7 text-center">{{ $item->alamat }}</div>
+                <div class="w-10 ">{{ $loop->iteration }}</div>
+                <div class="w-1/7 ">{{ $item->name }}</div>
+                <div class="w-1/7 text-center ">{{ $item->kode }}</div>
+                <div class="w-1/7 text-center ">{{ $item->mapel?->nama }}</div>
+                <div class="w-1/7 text-center ">{{ $item->no_pegawai ?? '-' }}</div>
+                <div class="w-1/7 text-center ">{{ $item->no_telp }}</div>
+                <div class="w-1/7 text-center ">{{ $item->alamat }}</div>
                 @if (Auth::user()->role == 'admin')
-                    <div class="flex gap-2">
+                    <div class="flex gap-2 w-1/7 justify-center">
                         <flux:button size="xs" variant="primary" color="amber"
                             wire:click='openEditModal({{ $item->id }})'>Ubah</flux:button>
                         {{-- <flux:modal.trigger name='delete-{{ $item->id }}'>
@@ -35,16 +35,16 @@
                         </flux:modal.trigger> --}}
                     </div>
                 @endif
-                @if (Auth::user()->role == 'admin')
-                    <flux:modal name="delete-{{ $item->id }}">
-                        <div class="mt-4">Yakin ingin menghapus Guru {{ $item->nama }}?</div>
-                        <div class="flex justify-end">
-                            <flux:button variant='danger' size="sm" wire:click='delete({{ $item->id }})'>Hapus
-                            </flux:button>
-                        </div>
-                    </flux:modal>
-                @endif
             </div>
+            @if (Auth::user()->role == 'admin')
+                <flux:modal name="delete-{{ $item->id }}">
+                    <div class="mt-4">Yakin ingin menghapus Guru {{ $item->nama }}?</div>
+                    <div class="flex justify-end">
+                        <flux:button variant='danger' size="sm" wire:click='delete({{ $item->id }})'>Hapus
+                        </flux:button>
+                    </div>
+                </flux:modal>
+            @endif
         @endforeach
 
         @if (Auth::user()->role == 'admin')
